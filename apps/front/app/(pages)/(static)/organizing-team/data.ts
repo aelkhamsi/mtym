fe6b4971ef14cef36032cf309093ecf7a722914b
Achraf <achrafelkhamsi@gmail.com>
2025-09-08 23:54:12 +0200

    update list of organizing team members

--- apps/front/app/(pages)/(static)/organizing-team/data.ts
+++ apps/front/app/(pages)/(static)/organizing-team/data.ts
@@ -3,7 +3,6 @@ export const organizingCommittee = [
   { name: "Omar Bennouna", portfolioSrc: "https://www.linkedin.com/in/omar-bennouna-a6b64b197/", imageSrc: "/organizing-team/omar_bennouna.webp" },
   { name: "Ilyass Bouazza", portfolioSrc: "https://www.linkedin.com/in/ilyass-bouazza-98030b290/", imageSrc: "/organizing-team/ilyass_bouazza.webp" },
   { name: "Hiba El Kasimi", portfolioSrc: "https://www.linkedin.com/in/hiba-el-kasimi/", imageSrc: "/organizing-team/hiba_el_kasimi.webp" },
-  { name: "Hamza Lasri", portfolioSrc: "https://www.linkedin.com/in/hamza-lasri-bb8347247/", imageSrc: "/organizing-team/hamza_lasri.webp" },
   { name: "Chaimaa Loutfi", portfolioSrc: "https://www.linkedin.com/in/chaimaa-loutfi-93bba821a/", imageSrc: "/organizing-team/chaimaa_loutfi.webp" },
   { name: "Kawtar Taik", portfolioSrc: "https://www.linkedin.com/in/kawtar-ta%C3%AFk-7544a11b9/", imageSrc: "/organizing-team/kawtar_taik.webp" },
   { name: "Malak Khaldouni", portfolioSrc: "https://www.linkedin.com/in/malak-khaldouni-383472253/", imageSrc: "/organizing-team/malak_khaldouni.webp" },
@@ -11,7 +10,6 @@ export const organizingCommittee = [
   { name: "Lina Bellahmidi", portfolioSrc: "https://www.linkedin.com/in/lina-bellahmidi-0b1251248/", imageSrc: "/organizing-team/lina_bellahmidi.webp" },
   { name: "Ali Noufli", portfolioSrc: "", imageSrc: "/organizing-team/ali_noufli.webp" },
   { name: "Amine Hbar", portfolioSrc: "https://www.linkedin.com/in/amine-hbar-0748a2246/", imageSrc: "/organizing-team/amine_hbar.webp" },
-  { name: "Jad Lasiri", portfolioSrc: "https://www.linkedin.com/in/jad-lasiri-6b7bb0292/", imageSrc: "/organizing-team/unknown.webp" },
   { name: "Ayman Amasrour", portfolioSrc: "https://www.linkedin.com/in/ayman-amasrour-1a280228a/", imageSrc: "/organizing-team/ayman_amasrour.webp" },
   { name: "Mohamed Reda El Messaoudi", portfolioSrc: "https://www.linkedin.com/in/mohamed-reda-el-messaoudi-bb21b2332/", imageSrc: "/organizing-team/mohamed_reda_el_messaoudi.webp" },
   { name: "FatimaZahra Moudakir", portfolioSrc: "https://www.linkedin.com/in/fatima-zahra-moudakir-615527246/", imageSrc: "/organizing-team/fatima_zahra_moudakir.webp" },
@@ -24,7 +22,6 @@ export const organizingCommittee = [
   { name: "Ismail Bouhaj", portfolioSrc: "https://www.linkedin.com/in/ismail-bouhaj-240745235/", imageSrc: "/organizing-team/ismail_bouhaj.webp" },
   { name: "Nour El Houda El Bouz", portfolioSrc: "https://www.linkedin.com/in/el-bouz-nour-el-houda-631425308/", imageSrc: "/organizing-team/nour_el_houda_el_bouz.webp" },
   { name: "Rania Daya", portfolioSrc: "https://www.linkedin.com/in/rania-daya-09b758296/", imageSrc: "/organizing-team/rania_daya.webp" },
-  { name: "Mouad Enoua", portfolioSrc: "https://www.linkedin.com/in/mouad-enoua-48a984327/", imageSrc: "/organizing-team/mouad_enoua.webp" },
   { name: "Manal Saoui", portfolioSrc: "https://www.linkedin.com/in/manal-saoui/", imageSrc: "/organizing-team/manal_saoui.webp" },
   { name: "Mohammed Younes El Gueddari", portfolioSrc: "https://www.linkedin.com/in/mohammed-younes-gueddari-4299b6147/?originalSubdomain=fr", imageSrc: "/organizing-team/mohammed_younes_gueddari.webp" },
 ];
