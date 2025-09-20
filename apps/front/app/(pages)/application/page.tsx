e65e8760438fddb13f23ece1594d3e647e57a78a
Achraf <achrafelkhamsi@gmail.com>
2025-09-20 13:02:53 +0200

    [front] restrict access to application form when applications are closed

--- apps/front/app/(pages)/application/page.tsx
+++ apps/front/app/(pages)/application/page.tsx
@@ -4,13 +4,27 @@ import { useAtomValue } from "jotai";
 import { ApplicationForm } from "./form/application-form";
 import { userAtom } from "@/app/store/userAtom";
 import { ProfileSkeleton } from "@mdm/ui"
+import { CLOSE_APPLICATIONS } from "config";
+import { useRouter } from "next/navigation";
 
 export default function ApplicationPage() {
   const user = useAtomValue(userAtom)
+  const router = useRouter();
+  const applicationComplete = user?.application && user?.application?.status?.status !== 'DRAFT'
+  const teamComplete = user?.team && user?.team?.users?.length >= 3 && user?.team?.users?.length <= 5 
 
-  if (user) {
-    return <ApplicationForm user={user} />
-  } else { 
-    return <ProfileSkeleton />
+  if (!user) {
+    return <ProfileSkeleton />;
   }
+  
+  if (!CLOSE_APPLICATIONS) {
+    return <ApplicationForm user={user} />;
+  }
+  
+  if (applicationComplete && teamComplete) {
+    return <ApplicationForm user={user} />;
+  }
+  
+  router.push("/profile/application");
+  return null;
 }
