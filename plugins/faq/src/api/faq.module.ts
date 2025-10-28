b94358da1304939534eb76512254bc779b80b867
Achraf <achrafelkhamsi@gmail.com>
2025-10-28 17:06:22 +0100

    add typeOrmModule import in faq plugin

--- plugins/faq/src/api/faq.module.ts
+++ plugins/faq/src/api/faq.module.ts
@@ -1,12 +1,17 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { FaqService } from './services/faq.service';
 import { FaqController } from './controllers/faq.controller';
+import { FaqEntity } from './entities/faq.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({})
 export class FaqModule {
   static register(): DynamicModule {
     return {
       module: FaqModule,
+      imports: [
+        TypeOrmModule.forFeature([FaqEntity]),
+      ],
       controllers: [FaqController],
       providers: [FaqService],
       exports: [FaqService],
