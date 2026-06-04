bd940d31411252ef96d057744957210808a37d4d
popopop - Ibrahim <ibrahim.essedyq@gmail.com>
2026-06-04 16:56:32 +0100

    fix importmap issue

--- apps/front/app/(cms)/admin/importMap.js
+++
@@ -1,8 +0,0 @@
-import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
-import { Logo as Logo_a7b9c3d2e1f4g5h6i7j8k9l0m1n2o3p4 } from '@/app/components/Logo'
-
-/** @type import('payload').ImportMap */
-export const importMap = {
-  '@payloadcms/next/rsc#CollectionCards': CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1,
-  '@/app/components/Logo#Logo': Logo_a7b9c3d2e1f4g5h6i7j8k9l0m1n2o3p4,
-}
