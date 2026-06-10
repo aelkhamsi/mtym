e461a36d14eb27f71ebb27d6e5dc46079d8f5611
Achraf El Khamsi <achrafelkhamsi@gmail.com>
2026-06-11 00:17:33 +0200

    feat: be more explicit in naming env variables

--- apps/api/src/config/s3.config.ts
+++ apps/api/src/config/s3.config.ts
@@ -1,12 +1,12 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('s3', () => ({
-  name: process.env.S3_BUCKET_NAME,
-  endpoint: process.env.S3_ENDPOINT,
-  region: process.env.S3_REGION ?? 'us-east-1',
+  name: process.env.MINIO_BUCKET_NAME,
+  endpoint: process.env.MINIO_ENDPOINT,
+  region: process.env.MINIO_REGION ?? 'us-east-1',
   forcePathStyle: true,
   credentials: {
-    accessKeyId: process.env.S3_ACCESS_KEY,
-    secretAccessKey: process.env.S3_SECRET_KEY,
+    accessKeyId: process.env.MINIO_ROOT_USER,
+    secretAccessKey: process.env.MINIO_ROOT_PASSWORD,
   },
 }));
