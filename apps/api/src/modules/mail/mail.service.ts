ab311063742961a6a2606cff05c2bd807779062d
Ibrahim Esseddyq <ibrahim.essedyq@gmail.com>
2026-06-25 18:33:44 +0000

    fix issue in import mailservice

--- apps/api/src/modules/mail/mail.service.ts
+++ apps/api/src/modules/mail/mail.service.ts
@@ -1,3 +1,4 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { User } from 'src/modules/user/entities/user.entity';
