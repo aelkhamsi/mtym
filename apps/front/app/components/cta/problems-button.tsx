6934c8d800cea7a8f5d33fa234d07bd48f3287f9
Achraf El Khamsi <achrafelkhamsi@gmail.com>
2026-07-14 14:43:19 +0200

    feat: publish mtym 2026 problems

--- apps/front/app/components/cta/problems-button.tsx
+++ apps/front/app/components/cta/problems-button.tsx
@@ -7,11 +7,11 @@ const ProblemsButton = () => {
   return (
     <Link
       className="flex max-w-fit items-center justify-center h-11 space-x-2 rounded-full border-[3px] border-[#f6a806] bg-white px-4 py-2 text-gray-700 shadow-md transition-colors hover:border-gray-500"
-      href="https://drive.google.com/file/d/1EIRuCYoFHq9bVw56pzNSy13bJ9mnZlob/view?usp=sharing"
+      href="https://drive.google.com/file/d/1tdx8-VT4JXpJxCjj-tEiRUteoT8RFG02/view?usp=sharing"
       target='_blank'
     >
       <BookIcon className='h-5 w-5'/>
-      <span>Problèmes 2025</span>
+      <span>Problèmes 2026</span>
     </Link>
   )
 }
