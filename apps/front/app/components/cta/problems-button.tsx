0a2f603f3c0d07b988d088201d22de7cc44314ae
Achraf El Khamsi <achrafelkhamsi@gmail.com>
2026-07-15 19:14:08 +0200

    feat: update link to final version of problems

--- apps/front/app/components/cta/problems-button.tsx
+++ apps/front/app/components/cta/problems-button.tsx
@@ -7,7 +7,7 @@ const ProblemsButton = () => {
   return (
     <Link
       className="flex max-w-fit items-center justify-center h-11 space-x-2 rounded-full border-[3px] border-[#f6a806] bg-white px-4 py-2 text-gray-700 shadow-md transition-colors hover:border-gray-500"
-      href="https://drive.google.com/file/d/1tdx8-VT4JXpJxCjj-tEiRUteoT8RFG02/view?usp=sharing"
+      href="https://drive.google.com/file/d/1eeZTcGhm8TxGbzeTz21NQk9g2g28efo_/view?usp=sharing"
       target='_blank'
     >
       <BookIcon className='h-5 w-5'/>
